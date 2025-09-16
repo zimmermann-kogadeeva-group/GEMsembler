bbb1203a81f63c344876c68afa3de7c5769385c6
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2025-09-16 15:33:34 +0200

    Added API section to the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'GEMsembler'
-copyright = '2025, Elena Matveishina, Bartosz Bartmanski'
-author = 'Elena Matveishina, Bartosz Bartmanski'
-release = '0.9.13'
+project = "GEMsembler"
+copyright = "2025, Elena Matveishina, Bartosz Bartmanski"
+author = "Elena Matveishina, Bartosz Bartmanski"
+release = "0.11.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["nbsphinx"]
+extensions = ["nbsphinx", "autoapi.extension"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+autoapi_dirs = ["../src"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
