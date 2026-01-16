5a03c4e1136c0406f8e0f3524d5c398de1b36caa
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2026-01-16 14:47:55 +0100

    Bump version 0.11.11 -> 0.11.12

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "GEMsembler"
 copyright = "2025, Elena Matveishina, Bartosz Bartmanski"
 author = "Elena Matveishina, Bartosz Bartmanski"
-release = "0.11.11"
+release = "0.11.12"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
